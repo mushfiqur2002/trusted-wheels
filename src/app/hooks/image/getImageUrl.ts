bece9b018410e7f76e8423ba7ec4fa6005beb047
mushfiqur2002 <mushfiqurnasim@gmail.com>
2026-05-24 23:26:12 +0600

    ....

--- src/app/hooks/image/getImageUrl.ts
+++ src/app/hooks/image/getImageUrl.ts
@@ -1,10 +1,10 @@
 import { storage } from "@/lib/appwrite";
 
 export const getImageUrl = (fileId: string) => {
-    const BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || ""
+    const BUCKET_ID =
+        process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || "";
 
-    return storage.getFileView(
-        BUCKET_ID,
-        fileId
-    )
-}
+    return storage
+        .getFileView(BUCKET_ID, fileId)
+        .toString();
+};
